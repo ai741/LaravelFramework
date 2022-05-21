55d9ddd72c738e57e4c7f9bb09ffeb34c1c24c4c
ai741 <link.4el@gmail.com>
2022-05-21 09:16:28 +0500

    Version - 1.0.9

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,10 @@ use \App\Http\Controllers\TestController;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
+
 //Практика на 03.05
+
 Route::view('page/main', 'page/news')->name("База");
 Route::view('page/news', 'page/onenews')->name("Одна новость");
 Route::view('page/auth', 'page/auth')->name("Авторизация");
